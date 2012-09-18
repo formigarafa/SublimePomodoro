576fdae05309cb5d62a516376f8783d8bc7e18b9
Rafael Santos <formigarafa@gmail.com>
2012-09-19 09:47:28 +1200

    refactored for smaller methods and for understanding

--- Pomodoro.py
+++ Pomodoro.py
@@ -11,31 +11,45 @@ class Pomodoro(threading.Thread):
 
 	def __init__(self):
 		threading.Thread.__init__(self)
+		self.start_time = None
 
 	def run(self):
 		Pomodoro.is_running = True
-		start_time = datetime.datetime.now()
-		d = datetime.datetime.now() - start_time
+		self.mark_start_time()
 		
-		while (Pomodoro.is_running and d.seconds < Pomodoro.MAX_SECONDS):
-				minutes = 24 - (d.seconds / 60)
-				seconds = (60 - d.seconds) % 60
-				if seconds == 0:
-					minutes = minutes + 1
-				sublime.set_timeout(
-					lambda: sublime.status_message('Pomodoro Time: [{0:02d}:{1:02d}]'.format(minutes , seconds)) , 100)
+		while (Pomodoro.is_running and ( not self.timer_expired() )):
+				self.display_ramaining_time()
 				time.sleep(1)
-				d = datetime.datetime.now() - start_time
 		
-		if Pomodoro.is_running:
-				sublime.set_timeout(
-					lambda: sublime.status_message('Pomodoro finished!') , 0)
-				sublime.set_timeout(
-					lambda: sublime.error_message('Pomodoro finished!') , 0)
-
+		if Pomodoro.is_running: # if finished instead of cancelled
+			self.status_message('Pomodoro finished!')
+			self.message_dialog('Pomodoro finished!')
 
 		Pomodoro.is_running = False
-		
+
+	def mark_start_time(self):
+		self.start_time = self.now()
+
+	def now(self):
+		return datetime.datetime.now()
+
+	def timer_expired(self):
+		return self.elapsed_time().seconds > Pomodoro.MAX_SECONDS
+
+	def elapsed_time(self):
+		return self.now() - self.start_time
+
+	def display_ramaining_time(self):
+		remaining_seconds = Pomodoro.MAX_SECONDS - self.elapsed_time().seconds
+		minutes = remaining_seconds / 60
+		seconds = remaining_seconds % 60
+		self.status_message( "Pomodoro Time: [{0:02d}:{1:02d}]".format(minutes , seconds))
+
+	def status_message(self, message):
+		sublime.set_timeout(lambda: sublime.status_message(message) , 0)
+
+	def message_dialog(self, message):
+		sublime.set_timeout(lambda: sublime.message_dialog(message), 0)
 
 class StartPomodoroTimerCommand(sublime_plugin.ApplicationCommand):
 	
@@ -47,12 +61,10 @@ class StartPomodoroTimerCommand(sublime_plugin.ApplicationCommand):
 	def is_enabled(self):
 		return not Pomodoro.is_running
 
-
 class StopPomodoroTimerCommand(sublime_plugin.ApplicationCommand):
 	def run(self):
 		sublime.status_message("Pomodoro timer stopped!")
 		Pomodoro.is_running = False
-
 				
 	def is_enabled(self):
 		return Pomodoro.is_running
